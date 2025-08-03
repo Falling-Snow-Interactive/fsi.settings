1bb13b7f31c8f60bec72158b3eb23ea9cb4a262a
Kira Dinan <kira.dinan@fallingsnowinteractive.ca>
2025-08-03 11:56:10 -0300

    adding asmdef

--- Editor/SettingsEditorUtility.cs
+++ Editor/SettingsEditorUtility.cs
@@ -1,3 +1,6 @@
+// using Fsi.Ui.Labels;
+
+using Fsi.Ui.Labels;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
@@ -17,6 +20,62 @@ namespace Fsi.Settings
 			SerializedProperty property = serializedObject.FindProperty(name);
 			PropertyField field = new(property);
 			return field;
+		}
+
+		public static VisualElement CreateSection(SerializedObject serializedObject, string title, string[] propertyNames)
+		{
+			var section = new Box
+			              {
+				              style =
+				              {
+					              marginTop = 0,
+					              marginLeft = 10,
+					              marginRight = 10,
+					              marginBottom = 0
+				              }
+			              };
+
+			var label = LabelUtility.Section(title);
+			section.Add(label);
+
+			foreach (string propName in propertyNames)
+			{
+				var prop = new PropertyField(serializedObject.FindProperty(propName));
+				prop.Bind(serializedObject);
+				section.Add(prop);
+			}
+
+			return section;
+		}
+		
+		public static VisualElement CreateIMGUISection(SerializedObject serializedObject, string title, string[] propertyNames)
+		{
+			var section = new VisualElement
+			              {
+				              style =
+				              {
+					              marginTop = 10,
+					              marginBottom = 10
+				              }
+			              };
+
+			var label = LabelUtility.Section(title);
+			section.Add(label);
+
+			var imguiContainer = new IMGUIContainer(() =>
+			                                        {
+				                                        serializedObject.Update();
+
+				                                        foreach (string propName in propertyNames)
+				                                        {
+					                                        EditorGUILayout.PropertyField(serializedObject.FindProperty(propName));
+					                                        serializedObject.ApplyModifiedProperties();
+				                                        }
+			                                        });
+
+			section.Add(imguiContainer);
+
+			return section;
 		}
 	}
 }
